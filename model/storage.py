9f21c3d9bd88290e3b925fcc03d3af4829974bbc
Wu Clan <jianhengwu0407@gmail.com>
2025-12-07 15:39:49 +0800

    Add interface implementations

--- model/storage.py
+++ model/storage.py
@@ -1,3 +1,5 @@
+import sqlalchemy as sa
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from backend.common.model import Base, UniversalText, id_key
@@ -8,12 +10,12 @@ class S3Storage(Base):
 
     __tablename__ = 's3_storage'
 
-    id: Mapped[id_key]
-    name: Mapped[str] = mapped_column(comment='存储名称')
-    endpoint: Mapped[str] = mapped_column(comment='终端节点')
-    access_key: Mapped[str] = mapped_column(comment='访问密钥')
-    secret_key: Mapped[str] = mapped_column(comment='密钥')
-    bucket: Mapped[str] = mapped_column(comment='存储桶')
-    prefix: Mapped[str | None] = mapped_column(default=None, comment='前缀')
-    region: Mapped[str | None] = mapped_column(default=None, comment='区域')
+    id: Mapped[id_key] = mapped_column(init=False)
+    name: Mapped[str] = mapped_column(sa.String(64), default='', comment='存储名称')
+    endpoint: Mapped[str] = mapped_column(sa.String(512), default='', comment='终端节点')
+    access_key: Mapped[str] = mapped_column(sa.String(512), default='', comment='访问密钥')
+    secret_key: Mapped[str] = mapped_column(sa.String(512), default='', comment='密钥')
+    bucket: Mapped[str] = mapped_column(sa.String(64), default='', comment='存储桶')
+    prefix: Mapped[str | None] = mapped_column(sa.String(256), default=None, comment='前缀')
+    region: Mapped[str | None] = mapped_column(sa.String(64), default=None, comment='区域')
     remark: Mapped[str | None] = mapped_column(UniversalText, default=None, comment='备注')
--- schema/storage.py
+++ schema/storage.py
@@ -12,8 +12,10 @@ class S3StorageSchemaBase(SchemaBase):
     endpoint: str = Field(description='终端节点')
     access_key: str = Field(description='访问密钥')
     secret_key: str = Field(description='密钥')
-    bucket: str = Field(description='存储痛')
+    bucket: str = Field(description='存储桶')
     prefix: str | None = Field(None, description='前缀')
+    region: str | None = Field(None, description='区域')
+    remark: str | None = Field(None, description='备注')
 
 
 class CreateS3StorageParam(S3StorageSchemaBase):
@@ -22,6 +24,12 @@ class CreateS3StorageParam(S3StorageSchemaBase):
 
 class UpdateS3StorageParam(S3StorageSchemaBase):
     """更新 S3 存储参数"""
+
+
+class DeleteS3StorageParam(SchemaBase):
+    """删除 S3 存储参数"""
+
+    pks: list[int] = Field(description='S3 存储 ID 列表')
 
 
 class GetS3StorageDetail(S3StorageSchemaBase):
